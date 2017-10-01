51bca38ba98ec4adcce62c66007d9d251ba12c2b
fdepedis <flavio.depedis@gmail.com>
2017-10-01 16:00:17 +0200

    Add InventoryCursorAdapter

---
+++ app/src/main/java/InventoryCursorAdapter.java
@@ -0,0 +1,27 @@
+import android.content.Context;
+import android.database.Cursor;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+
+/**
+ * Created by flavio.depedis on 01/10/2017.
+ */
+
+public class InventoryCursorAdapter extends CursorAdapter {
+
+
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+    }
+}
